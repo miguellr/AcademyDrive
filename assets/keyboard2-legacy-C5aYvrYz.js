e5bdcefa9c4b886e2681752fa41bb704503a4403
mike <lauri>
2024-05-30 23:14:19 +0200

    Updates

--- assets/keyboard2-legacy-C5aYvrYz.js
+++ assets/keyboard2-legacy-C5aYvrYz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BzNOnWMT.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Cn41E7F_.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
