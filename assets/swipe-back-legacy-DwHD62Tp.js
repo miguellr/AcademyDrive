eba2ca105720c75b81d379f1632abc1cc6964828
mike <lauri>
2024-05-31 13:47:03 +0200

    Updates

--- assets/swipe-back-legacy-DwHD62Tp.js
+++ assets/swipe-back-legacy-DwHD62Tp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3hnJ-2nw.js","./index3-legacy-Bp4mU1S7.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-C1sHMKax.js","./index3-legacy-Dcy-LMZ_.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
