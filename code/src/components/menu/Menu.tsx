b065f6532c3848d05e79c0c6073fa3ac018e1ba5
mike <lauri>
2024-05-31 16:51:54 +0200

    mejoras menu y logo

--- code/src/components/menu/Menu.tsx
+++ code/src/components/menu/Menu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IonList, IonItem, IonLabel, IonIcon, IonListHeader, IonNote, IonMenuToggle } from '@ionic/react';
 import './Menu.css'
+import { callOutline } from 'ionicons/icons';
 
 interface MenuProps {
     onSectionSelect: (section: Sections) => void;
@@ -9,22 +10,40 @@ interface MenuProps {
 
 // <IonListHeader>Amarelle Academy</IonListHeader>
 // <IonNote>Choose one below to see a demo</IonNote>
+/*
+
+            <IonListHeader slot='bottom'>Redes Sociales
+                <IonItem className="custom-menu-item" lines="none">
+
+                    <a href={"url"} target="_blank" rel="noopener noreferrer">
+                        <IonIcon icon={callOutline} />
+                    </a>
+                </IonItem>
+            </IonListHeader> */
 const Menu: React.FC<MenuProps> = ({ onSectionSelect, menuData }) => {
     return (
-        <IonList className="custom-menu">
-            {menuData.map((section, index) => (
-                <IonMenuToggle key={index} autoHide={false}>
-                    <IonItem
-                        key={index}
-                        button
-                        onClick={() => onSectionSelect(section)}
-                        className="custom-menu-item"
-                    >
-                        <IonLabel>{section.name}</IonLabel>
-                    </IonItem>
+        <>
+            <IonList className="custom-menu">
+                <IonMenuToggle autoHide={false} className="test">
+                    <IonListHeader className='custom-menu-header'>
+                        <img src="/AcademyDrive/logo.png" alt="logo" className="custom-logo" />
+                        <h4>Amarelle Academy</h4>
+                    </IonListHeader>
+                    {menuData.map((section, index) => (
+
+                        <IonItem
+                            key={index}
+                            button
+                            onClick={() => onSectionSelect(section)}
+                            className="custom-menu-item"
+                        >
+                            <IonLabel>{section.name}</IonLabel>
+                        </IonItem>
+
+                    ))}
                 </IonMenuToggle>
-            ))}
-        </IonList>
+            </IonList>
+        </>
     );
 };
 
